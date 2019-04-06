29eb79ced5004e2123ed02afc053149dda514cc2
geopatrikis <geopatrikis12@gmail.com>
2019-04-06 13:33:39 +0300

    6/4/2019 changes to Broker Pub Sub by Pat

--- Subscriber.java
+++ Subscriber.java
@@ -6,44 +6,47 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Subscriber {
-	
-	public static void main(String[] args) throws ClassNotFoundException, InterruptedException {
-		new Subscriber().startClient();
-	}
-	Message current =new Message(2,"asdasd ","asda");
-	public void startClient() throws ClassNotFoundException, InterruptedException {
-		Socket subSocket = null;
-		ObjectOutputStream out = null;
-		ObjectInputStream in = null;
-		while (true) {
-			try {
-				subSocket = new Socket(InetAddress.getByName("localhost"), 10240);
-				out = new ObjectOutputStream(subSocket.getOutputStream());
-				in = new ObjectInputStream(subSocket.getInputStream());
-				out.writeObject(new Message(2, "035", " sub inquiry"));
-				out.flush();
-				Message temp = (Message) in.readObject();
-				if (!current.data.equals(temp.data)) {
-					current.data = temp.data;
-					System.out.println(temp);
-				}
-			} catch (Exception e) {
-				System.out.println("Connection with server is not ready yet ");
-				Thread.sleep(2000);
-				continue;
-			}
-		}
 
-	}
+    public static void main(String[] args) throws ClassNotFoundException, InterruptedException {
+        new Subscriber().startClient();
+    }
 
-	public void register(Broker broker,Topic topic) {
-		broker.acceptConnection(this);
-		broker.HM.get(topic);
-	}
-	
-	public void disconnect(Broker broker, Topic topic) {
-		broker.registeredSubscribers.remove(topic);
-	}
-	
-	public void visualiseData(Topic topic, Value value) {}
+    Message current = new Message(2, "asdasd ", "asda");
+
+    public void startClient() throws ClassNotFoundException, InterruptedException {
+        Socket subSocket = null;
+        ObjectOutputStream out = null;
+        ObjectInputStream in = null;
+        while (true) {
+            try {
+                subSocket = new Socket(InetAddress.getByName("localhost"), 10240);
+                out = new ObjectOutputStream(subSocket.getOutputStream());
+                in = new ObjectInputStream(subSocket.getInputStream());
+                out.writeObject(new Message(2, "035", " sub inquiry"));
+                out.flush();
+                Message temp = (Message) in.readObject();
+                if (!current.data.equals(temp.data)) {
+                    current.data = temp.data;
+                    System.out.println(temp);
+                }
+            } catch (Exception e) {
+                System.out.println("Connection with server is not ready yet ");
+                Thread.sleep(2000);
+                continue;
+            }
+        }
+
+    }
+
+    public void register(Broker broker, Topic topic) {
+        broker.acceptConnection(this);
+        broker.HM.get(topic);
+    }
+
+    public void disconnect(Broker broker, Topic topic) {
+        broker.registeredSubscribers.remove(topic);
+    }
+
+    public void visualiseData(Topic topic, Value value) {
+    }
 }
